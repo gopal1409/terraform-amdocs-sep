4fb4e5dbeb41c74ebc1acaf1853b3fa3c2a46467
gopal das <gopal1409@gmail.com>
2023-09-05 10:21:07 +0530

    terraform new provider

--- provider.tf
+++ provider.tf
@@ -1,14 +1,25 @@
 # first we need to provide the provider block
 provider "azurerm" {
-  version = "=2.36.0"
-  features {
-    
+  features {}
+}
+##3as per terraform 14 version we are providing this provider
+terraform {
+  required_providers {
+    azurerm={
+      source = "hashicorp/azurerm"
+      #version = "2.40.0"
+    } 
   }
 }
+###but if you add another provider. random generator local null data
+###we created this block and inside this provider block we have given a cloud service provider name
+#erraform init it will download the plugin required to communicate with the cloud service provider. 
+###when you want to share the code never share this plugin. 
+#if someone want to use the code redo terraform init
 
 resource "azurerm_resource_group" "rg" {
-    ###change the resource group name
-  name = "myrg-gopal"
+  ###change the resource group name
+  name     = "myrg-gopal"
   location = "eastus"
 }
 
